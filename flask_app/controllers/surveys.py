2b4b994b2e6b93df7a5aaa15ba7985e0055d7bd4
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-07 16:31:46 -0700

    Survey Complete

--- flask_app/controllers/surveys.py
+++ flask_app/controllers/surveys.py
@@ -6,5 +6,17 @@ from flask_app.models.model_survey import Survey
 
 @app.route('/')
 def survey():
-    return render_template('survey_results.html')
+    return render_template('survey.html')
 
+@app.route('/create', methods=['POST'])
+def create_survey():
+    session['name'] = request.form['name']
+    session['location'] = request.form['dojo_location']
+    session['fav_language'] = request.form['fav_language']
+    session['comments'] = request.form['comments']
+    print(session['name'])
+    return redirect('/survey_results')
+
+@app.route('/survey_results')
+def display_results():
+    return render_template('survey_results.html')
